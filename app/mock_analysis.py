f752d0fe1c10c0a9bfedbd3d2aea8cbe385ad3f5
FilipMasek1 <masek-555@seznam.cz>
2026-09-17 09:07:16 +0200

    Refactor Mock_Analyza function and improve imports

--- app/mock_analysis.py
+++ app/mock_analysis.py
@@ -1,5 +1,7 @@
 import yfinance as yf
 import pandas as pd
+from pathlib import Path
+os.chdir(Path(__file__).resolve().parent)
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
